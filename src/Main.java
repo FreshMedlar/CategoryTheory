50861316a2d7e3e3b4e56adf2719f9a4e7e9cd44
EmaSega <68239245+FreshMedlar@users.noreply.github.com>
2021-05-10 18:50:25 +0200

    main and test1

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,29 @@
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Objects;
+import java.util.function.Function;
+
 public class Main {
 
+    public static <T> T id(T obj) {
+        return obj;
+    }
+
+    @Contract(pure = true)
+    public static <A, B, C> Function<A, C> compose(Function<B, C> g, Function<A, B> f) {
+        return obj -> g.apply(f.apply(obj));
+    }
+
+    public static void main(String[] args) {
+        System.out.println("1 == id 1 : " + (1 == id(1)));
+
+        Integer number = 100;
+
+        System.out.println("id . toString == toString : " +
+                Objects.equals(compose(Main::id, Object::toString).apply(number), number.toString()));
+
+        System.out.println("toString . id == toString : " +
+                Objects.equals(compose(Object::toString, Main::id).apply(number), number.toString()));
+    }
 }

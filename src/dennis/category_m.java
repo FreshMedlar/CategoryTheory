8bcbaa611084aa8c20836a03c15ded43438a2379
EmaSega <68239245+FreshMedlar@users.noreply.github.com>
2021-05-19 22:13:09 +0200

    main and test1

--- src/dennis/category_m.java
+++ src/dennis/category_m.java
@@ -7,23 +7,27 @@ import java.util.function.Function;
 public abstract class category_m <E>{
     String id ="";
     public ArrayList<element<E>> element_list = new ArrayList<element<E>>();
-public static Function compose(Function f1,Function f2){
-    f1=f1.andThen(f2);
-    return f1 ;
-}
-public  void add(element i){
-    this.element_list.add(i);
-}
-public void add(E i){
-    element<E> el = new element<E>(i);
-    add(el);
-}
-public element get(E i){
-    for(int a=0;a<element_list.size();a++){
-        if(element_list.get(a).value==i){
-            return element_list.get(a);
-        }
+
+    public static Function compose(Function f1,Function f2){
+        f1=f1.andThen(f2);
+        return f1 ;
     }
+
+    public  void add(element i){
+        this.element_list.add(i);
+    }
+
+    public void add(E i){
+        element<E> el = new element<E>(i);
+        add(el);
+    }
+
+    public element get(E i){
+        for(int a=0;a<element_list.size();a++){
+            if(element_list.get(a).value==i){
+                return element_list.get(a);
+            }
+        }
     return new element();
-}
+    }
 }
